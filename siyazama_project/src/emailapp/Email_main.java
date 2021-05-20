ea602b6fde4bbb4fbcf25debbeffa2fd236a2498
Thami618 <thamngcobo1998@gmail.com>
2021-05-20 18:47:38 +0200

    final commit on the App

--- siyazama_project/src/emailapp/Email_main.java
+++ siyazama_project/src/emailapp/Email_main.java
@@ -4,6 +4,12 @@ public class Email_main {
 
 	public static void main(String[] args) {
 		Email_obj eml = new Email_obj("Ntokozo", "Mkhize");
+
+		
+		System.out.println(eml.showInfo());
+		
+	
+		
 	}
 
 }
--- siyazama_project/src/emailapp/Email_obj.java
+++ siyazama_project/src/emailapp/Email_obj.java
@@ -7,28 +7,34 @@ public class Email_obj {
 	private String lastname;
 	private String password;
 	private String department;
-	private int mailboxcapacity;
+	private int mailboxcapacity = 500;
 	private int defaultpasswrdlength = 10;
 	private String alternativeemail;
+	private String email;
+	private String company_suffix = "siyazama.com";
 	
 	//Constructor to recieve the first name and last name
 	public Email_obj(String firstname, String lastname) {
 		this.firstname = firstname;
 		this.lastname = lastname;
-		System.out.println("Email created :" + this.firstname + " " + this.lastname);
+		
 		
 		//Call method asking for deptment - return the dept
 		this.department = setdept();
-		System.out.println("Department: " + this.department);
+		//System.out.println("Department: " + this.department);
 		
 		//Call a method  that returns a random password
 		this.password = randomPasswrd(defaultpasswrdlength);
 		System.out.println("Your password is  " + this.password);
+		
+		//Combine elements to generate email
+		email = firstname.toLowerCase() + "." + lastname.toLowerCase() + "@" + department + "." + company_suffix;
+		//System.out.println("Your email: " + email);
 	}
 	
 	//ask for a department
 	private String setdept() {
-		System.out.println("DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code:");
+		System.out.println("New Worker: " + firstname + "  " + lastname + "\nDEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code:");
 		Scanner in  = new Scanner(System.in);
 		int depChoice = in.nextInt();
 		if(depChoice == 1) {
@@ -55,9 +61,33 @@ public class Email_obj {
 		}
 		return new String(passwrd);
 	}
-	
 	//set mailbox capacity
-	
+	public void setmailboxcap(int capacity) {
+		this.mailboxcapacity = capacity;
+	}
+	//set alternative email
+	public void set_altemail(String altEmail) {
+		this.alternativeemail = altEmail;
+	}
 	//change password
+	public void changpasswrd(String passwd) {
+		this.password = passwd;
+	}
+	//Get methods
+	public int getMailboxcapacity() {
+		return mailboxcapacity;
+	}
+	public String getAltemail() {
+		return alternativeemail;
+	}
+	public String getPasswrd() {
+		return password;
+	}
+	
+	public String showInfo() {
+		return "DIPLAY NAME: " + firstname + " " + lastname +
+				"\nCOMPANY EMAIL: " + email +
+				"\nMAILBOX CAPACITY; " + mailboxcapacity + "mb";
+	}
 
 }
