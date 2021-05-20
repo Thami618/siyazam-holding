07447129d932a9e23568edb449de183b852731ad
Thami618 <thamngcobo1998@gmail.com>
2021-05-20 16:33:29 +0200

    Getting random passwrds

--- siyazama_project/src/emailapp/Email_main.java
+++ siyazama_project/src/emailapp/Email_main.java
@@ -3,8 +3,7 @@ package emailapp;
 public class Email_main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Email_obj eml = new Email_obj("Ntokozo", "Mkhize");
 	}
 
 }
--- siyazama_project/src/emailapp/Email_obj.java
+++ siyazama_project/src/emailapp/Email_obj.java
@@ -1,6 +1,63 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class Email_obj {
+	private String firstname;
+	private String lastname;
+	private String password;
+	private String department;
+	private int mailboxcapacity;
+	private int defaultpasswrdlength = 10;
+	private String alternativeemail;
 	
+	//Constructor to recieve the first name and last name
+	public Email_obj(String firstname, String lastname) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+		System.out.println("Email created :" + this.firstname + " " + this.lastname);
+		
+		//Call method asking for deptment - return the dept
+		this.department = setdept();
+		System.out.println("Department: " + this.department);
+		
+		//Call a method  that returns a random password
+		this.password = randomPasswrd(defaultpasswrdlength);
+		System.out.println("Your password is  " + this.password);
+	}
+	
+	//ask for a department
+	private String setdept() {
+		System.out.println("DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code:");
+		Scanner in  = new Scanner(System.in);
+		int depChoice = in.nextInt();
+		if(depChoice == 1) {
+			return "Sales";
+		}
+		else if (depChoice == 2) {
+			return "Development";
+		}
+		else if (depChoice == 3) {
+			return "Accounting";
+		}
+		else {
+			return "";
+		}
+	}
+
+	//generate a random password
+	private String randomPasswrd(int length) {
+		String passwrdset = "ABCDEFGHIJKLMNOPSTUVWXYX123456!@#$%";
+		char[] passwrd = new char [length];
+		for(int i = 0; i<length; i++){
+			int rand = (int)(Math.random() * passwrdset.length());
+			passwrd[i] = passwrdset.charAt(rand);
+		}
+		return new String(passwrd);
+	}
+	
+	//set mailbox capacity
+	
+	//change password
 
 }
